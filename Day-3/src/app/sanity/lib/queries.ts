739a006b97c52f9a8ea8c88579504f0febe96695
SeharFatima <160278458+SehrishAkbar@users.noreply.github.com>
2025-02-11 23:38:27 +0500

    Update queries.ts

--- Day-3/src/app/sanity/lib/queries.ts
+++ Day-3/src/app/sanity/lib/queries.ts
@@ -3,3 +3,13 @@ import { groq } from "next-sanity";
 
 export const allproducts = groq`*[_type =="product"]`;
 export const four = groq `*[_type == "product"][0-3]`;
+  _id,
+  name,
+  description,
+  price,
+  quantity
+          "categoryName": category->name,
+          "slug": slug.current,
+          "imageUrl": image.asset->url,
+          rating
+           }`;
